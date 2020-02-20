09e34da838819d63a3832623d1ba8e8b78408d2f
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-20 13:29:21 -0500

    Initial Commit

---
+++ On The Map/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Michal Hus on 2/20/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
