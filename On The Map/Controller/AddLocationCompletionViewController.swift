457317bc30a6dc0aea6a15e3c9c0eb6bfba89c03
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-23 07:53:06 -0500

    Added add location navigation flow with use of new nav controller. (#6)

---
+++ On The Map/Controller/AddLocationCompletionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddLocationCompletionViewController.swift
+//  On The Map
+//
+//  Created by Michal Hus on 2/23/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationCompletionViewController: UIViewController {
+   
+    @IBAction func addLocation(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ On The Map/Controller/AddLocationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddLocationVC.swift
+//  On The Map
+//
+//  Created by Michal Hus on 2/23/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+    @IBAction func cancelOut(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
