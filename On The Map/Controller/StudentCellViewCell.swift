ba2dadf926ab07940eba864037b7384236da6ba4
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-24 13:59:56 -0500

    Added response handling and its display in the Table View

--- On The Map/Controller/StudentCellViewCell.swift
+++ On The Map/Controller/StudentCellViewCell.swift
@@ -11,4 +11,15 @@ import UIKit
 class StudentCellViewCell: UITableViewCell {
     @IBOutlet weak var studentName: UILabel!
     @IBOutlet weak var studentLink: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        studentName.text = ""
+        studentLink.text = ""
+    }
+    
+    func configure(with student: Student) {
+        studentName.text = student.firstName + " " + student.lastName
+        studentLink.text = student.mediaURL
+    }
 }
--- On The Map/Controller/TableViewController.swift
+++ On The Map/Controller/TableViewController.swift
@@ -10,28 +10,30 @@ import UIKit
 
 class StudentsLocationTableViewController: UITableViewController {
     
-//    let memes:[String] = ["bobby", "tobby", "hobby"]
+    var students: [Student] = []
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath)as! StudentCellViewCell
-//     
-////       Client.getStudentsLocation { (jsonPayload, error) in
-//////        print(jsonPayload)
-//////        print("HI")
-//////        let studentInfo = jsonPayload?[(indexPath as NSIndexPath).row]
-////        }
-//        
-//        let meme = memes[(indexPath as NSIndexPath).row]
-//        
-////        cell.studentLink =
-////        cell.studentName = meme[indexPath]
-//        
-//        // Set the text and image
-////        cell.textLabel?.text = combinedMemeText
-////        cell.imageView?.image = meme.memedImage
-////
-//        
-//        return cell
-//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Client.getStudentsLocation { (response, error) in
+            guard let response = response, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.students = response.results
+                self.tableView.reloadData()
+            }
+        }
+    }
     
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath)as! StudentCellViewCell
+        let student = students[indexPath.row]
+        cell.configure(with: student)
+        return cell
+    }
 }
