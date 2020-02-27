0317c06fe2717c5c1549d29e5e194ebc5373cbf5
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-27 11:15:07 -0500

    Updated mapView display on the complete add location view controller that displays one one pin that will be added after tapping finish.

--- On The Map/Controller/AddLocationCompletionViewController.swift
+++ On The Map/Controller/AddLocationCompletionViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class AddLocationCompletionViewController: UIViewController {
+class AddLocationCompletionViewController: UIViewController, MKMapViewDelegate {
    
     var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()
     var link:String = ""
     var mapString: String = ""
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     @IBAction func addLocation(_ sender: Any) {
         Client.addStudentLocation(mapString: mapString, mediaURL: link, latitude: coordinate.latitude, longitude: coordinate.longitude) { (success: Bool, error:Error?) in
             guard success, error == nil else {
@@ -24,6 +26,39 @@ class AddLocationCompletionViewController: UIViewController {
             }
             self.dismiss(animated: true, completion: nil)
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        self.pinLocatinDisplay()
+    }
+    
+    func pinLocatinDisplay() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "\(mapString)"
+        self.mapView.addAnnotation(annotation)
+    }
+    
+    // MARK: - MKMapViewDelegate
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard annotation is MKPointAnnotation else { return nil }
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
     }
     
 }
